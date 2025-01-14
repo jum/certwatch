daa7073eef5b96b6223ecdf9fbf1b0931dcf36d7
Jens-Uwe Mager <jum@anubis.han.de>
2025-01-14 15:24:25 +0100

    Add a cert reload command for debugging.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go run github.com/cespare/reflex -d none -s -g .env -- bash -c ". .env; go run github.com/cespare/reflex -d none -s -G .env -G \$CERTDIR -- go run . -debug -redisurl=\$REDISURL -certdir=\$CERTDIR \$CERTS"
+go run github.com/cespare/reflex -d none -s -g .env -- bash -c ". .env; go run github.com/cespare/reflex -d none -s -G .env -G \$CERTDIR -- go run . -debug -cmd='echo cert reload' -redisurl=\$REDISURL -certdir=\$CERTDIR \$CERTS"
