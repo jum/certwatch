3c930733ac7c3a514e6d648ec75481bda8ab34e9
Jens-Uwe Mager <jum@anubis.han.de>
2025-05-08 12:32:31 -0700

    Bump deps, switch to the go1.24 tools mechanism.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go run github.com/cespare/reflex -d none -s -g .env -- bash -c ". .env; go run github.com/cespare/reflex -d none -s -G .env -G \$CERTDIR -- go run . -debug -cmd='echo cert reload' -redisurl=\$REDISURL -certdir=\$CERTDIR \$CERTS"
+go tool reflex -d none -s -g .env -- bash -c ". .env; go tool reflex -d none -s -G .env -G \$CERTDIR -- go run . -debug -cmd='echo cert reload' -redisurl=\$REDISURL -certdir=\$CERTDIR \$CERTS"
